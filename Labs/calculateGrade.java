f8993c37f6ce286049bf0b1f55450e190c71fd3a
Tom Armstrong <77870646+tarmstrong2021@users.noreply.github.com>
2021-03-08 20:39:58 -0500

    Add files via upload

---
+++ Labs/calculateGrade.java
@@ -0,0 +1,24 @@
+
+public class calculateGrade {
+
+	public char letterGrade (double num) {
+		
+		char finalGrade;
+		
+		if (num < 60)
+			finalGrade = 'F';
+		else if (num >= 60 && num <70)
+			finalGrade = 'D';
+		else if (num >=70 && num <80)
+			finalGrade = 'C';
+		else if (num >=80 && num <90)
+			finalGrade = 'B';
+		else 
+			finalGrade = 'A';
+		
+		
+		return finalGrade;
+		
+	}
+	
+}
